55f1a550cc986936f77fbf894c3a5ee13981c379
ThornsH <abrahamm9986@gmail.com>
2021-11-02 16:54:13 -0600

    Update

--- Práctica_5.cpp
+++ Práctica_5.cpp
@@ -14,8 +14,9 @@ bool vacia (struct Nodo *l);
 void anula (struct Nodo *&l);
 void insertar_i(struct Nodo *&l, TIPO_DATO d);
 void insertar_p(struct Nodo *&l, TIPO_DATO d, int posicion, int elementos);
-void eliminar(struct Nodo *&l, int posicion);
+void eliminar(struct Nodo *&l,int pos, int elementos);
 void imprimir (struct Nodo *l);
+Nodo *anterior(Nodo *lista, unsigned int pos);
 
 
 int main ()
@@ -29,16 +30,24 @@ int main ()
   TIPO_DATO valor;
 
   do
-    {
-      imprimir (ancla);
+    { 
+      //system("clear");
       cout<<"                      Menu\n"<<'\n'
-            <<"1.- Vacia"<<'\n'
-            <<"2.- Insertar elemento al inicio"<<'\n'
-            <<"3.- Insertar elemento por posicion"<<'\n'
-            <<"4.- Eliminar"<<'\n'
-            <<"5.- Anular"<<'\n'
-            <<"0.- Salir"<<'\n'
-            <<"\nSeleccione la opcion deseada -> ";
+            <<"\t1.- Vacia"<<'\n'
+            <<"\t2.- Insertar elemento al inicio"<<'\n'
+            <<"\t3.- Insertar elemento por posicion"<<'\n'
+            <<"\t4.- Eliminar"<<'\n'
+            <<"\t5.- Anular"<<'\n'
+            <<"\t0.- Salir"<<'\n';
+
+            if(vacia (ancla)){
+              cout << "" << '\n';
+            }
+            else{
+              imprimir(ancla);
+            }
+
+            cout<<"\tSeleccione una opción ~ ";
       cin >> opcion;
 
       switch (opcion)
@@ -68,19 +77,16 @@ int main ()
 	  insertar_p(ancla, valor, posicion, elementos);
 	  break;
 	case 4:
-	  /*
-	      Implementar codigo para eliminar por posicion
-	  */
+    cout << "Dame la posición a eliminar: ";
+	  cin >> posicion;
+    eliminar(ancla,posicion,elementos);
 	  break;
-
 	case 5:
 	  anula(ancla);
 	  break;
-
 	default:
 	  cout << "Opcion Incorrecta!!!" << '\n';
 	  break;
-
 	}
     }
   while (opcion != 0);
@@ -116,32 +122,32 @@ void insertar_i(struct Nodo *&l, TIPO_DATO d){
 }
 
 void insertar_p(struct Nodo *&l, TIPO_DATO d, int pos,int elementos){
+
     struct Nodo *temp = l;
     struct Nodo *aux = new struct Nodo;
 
     if (pos > elementos or pos < 0){
       cout << "¡Posición inválida!";
-    }
-
-    for(int i = 0; i<pos-1; i++) {
-      temp = temp->siguiente;
-    }
+    }else{
+      for(int i = 0; i<pos-1; i++){
+        temp = temp->siguiente;
+      }
     aux->dato = d;
     aux->siguiente = temp->siguiente;
     temp->siguiente = aux;
+  }
 }
 
 void imprimir (struct Nodo *l)
 {
-  cout << "ancla-> ";
+  cout << "\tAncla-> ";
   while (l != nullptr)
     {
       cout << l->dato << "-> ";
       l = l->siguiente;
     }
-  cout << "nullptr" << '\n' << '\n';
+  cout << "NULL" << '\n' << '\n';
 }
-
 
 void anula (struct Nodo *&l)
 {
@@ -155,6 +161,38 @@ void anula (struct Nodo *&l)
       delete l;
       l = nullptr;
     }
-
 }
 
+void eliminar(struct Nodo *&l, int pos, int elementos){
+
+    struct Nodo *temp = l;
+    struct Nodo *temp2 = nullptr;
+
+    if(vacia(l)){
+      cout << "¡La Lista esta vacia!" << '\n';
+    }
+    else{
+        if (pos > elementos or pos < 0){
+          cout << "¡Posición inválida!";
+        }else{
+          for(int i = 0; i<pos-1; i++){
+            temp = temp->siguiente;
+          }
+        cout << temp->siguiente << '\n';
+        cout << temp2->siguiente << '\n';
+        temp2->siguiente = temp->siguiente;
+        cout << temp2->siguiente << '\n';
+        }
+    }
+}
+
+Nodo *anterior(Nodo *lista, unsigned int pos){
+    if (pos == 0){
+        return nullptr;
+    }
+    while(lista != nullptr and pos > 1){
+        lista = lista->siguiente;
+        pos--;
+    }
+    return lista;
+}
